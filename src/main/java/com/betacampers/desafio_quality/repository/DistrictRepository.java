a376c98de7162eabde615471734f076d70fcc1b3
pealmeida-meli <pedro.almeida@mercadolivre.com>
2022-07-13 15:17:18 -0300

    adiciona retorno da área dos cômodos

--- src/main/java/com/betacampers/desafio_quality/repository/DistrictRepository.java
+++ src/main/java/com/betacampers/desafio_quality/repository/DistrictRepository.java
@@ -3,10 +3,24 @@ package com.betacampers.desafio_quality.repository;
 import com.betacampers.desafio_quality.model.District;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Repository
 public class DistrictRepository implements IDistrictRepository {
+    private Map<UUID, District> districts;
+
+    public DistrictRepository() {
+        districts = new ArrayList<District>() {{
+            add(new District(UUID.randomUUID(), "Centro", new BigDecimal("10")));
+            add(new District(UUID.randomUUID(), "Sossego", new BigDecimal("10")));
+            add(new District(UUID.randomUUID(), "Passo D'areia", new BigDecimal("10")));
+        }}.stream().collect(Collectors.toMap(District::getDistrictId, x -> x));
+    }
+
     @Override
     public District getById(UUID districtId) {
         return null;
--- src/main/java/com/betacampers/desafio_quality/service/PropertyService.java
+++ src/main/java/com/betacampers/desafio_quality/service/PropertyService.java
@@ -1,14 +1,21 @@
 package com.betacampers.desafio_quality.service;
 
 import com.betacampers.desafio_quality.dto.RoomResponseDto;
+import com.betacampers.desafio_quality.model.Property;
+import com.betacampers.desafio_quality.repository.IPropertyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class PropertyService implements IPropertyService {
+    @Autowired
+    private IPropertyRepository repository;
+
     @Override
     public Double getPropertyArea(UUID propertyId) {
         return null;
@@ -26,6 +33,7 @@ public class PropertyService implements IPropertyService {
 
     @Override
     public List<RoomResponseDto> getRoomsArea(UUID propertyId) {
-        return null;
+        Property property = repository.getById(propertyId);
+        return property.getPropRooms().stream().map(RoomResponseDto::new).collect(Collectors.toList());
     }
 }
