b8f2a9c54a5b99c9e8a42ae1d633502e3dc7dcd4
˜Thiago <thiago.sordi@mercadolivre.com>
2022-07-13 15:23:49 -0300

    Calculo do valor

--- src/main/java/com/betacampers/desafio_quality/repository/PropertyRepository.java
+++ src/main/java/com/betacampers/desafio_quality/repository/PropertyRepository.java
@@ -15,6 +15,8 @@ public class PropertyRepository implements IPropertyRepository {
 
     private void createProperties(){
 
+        properties = new HashMap<>();
+
         District d1 = new District(UUID.randomUUID(), "Barreiros", new BigDecimal(4500));
         District d2 = new District(UUID.randomUUID(), "Campinas", new BigDecimal(5000));
         District d3 = new District(UUID.randomUUID(), "Kobrasol", new BigDecimal(4900));
@@ -24,20 +26,20 @@ public class PropertyRepository implements IPropertyRepository {
                 new Room("Quarto de casal", 3.5, 4.6), new Room("Cozinha", 3.6, 4.8),
                 new Room("Banheiro", 1.8, 2.4)));
 
-        Property p1 = new Property(UUID.randomUUID(), "Casa A", d1, rooms);
+        Property p1 = new Property(UUID.fromString("35d75bed-7a67-4c5e-86f6-88f35f4d9a39"), "Casa A", d1, rooms);
 
 
         rooms = new ArrayList<>(Arrays.asList(new Room("Quarto", 2.5, 4.2),
                 new Room("Cozinha", 2.5, 3.0),
                 new Room("Banheiro", 1.5, 2.2)));
 
-        Property p2 = new Property(UUID.randomUUID(), "Apartamento 12", d2, rooms);
+        Property p2 = new Property(UUID.fromString("46a7205b-3838-4021-974b-f084b1d2d71c"), "Apartamento 12", d2, rooms);
 
         rooms = new ArrayList<>(Arrays.asList(new Room("Quarto", 3.5, 3.2),
                 new Room("Cozinha", 2.5, 3.2),
                 new Room("Banheiro", 2.0, 1.6)));
 
-        Property p3 = new Property(UUID.randomUUID(), "Apartamento 08", d3, rooms);
+        Property p3 = new Property(UUID.fromString("11cdf800-981a-4be0-98f7-af9fbb558c0f"), "Apartamento 08", d3, rooms);
 
         properties.put(p1.getPropId(), p1);
         properties.put(p2.getPropId(), p2);
--- src/main/java/com/betacampers/desafio_quality/service/PropertyService.java
+++ src/main/java/com/betacampers/desafio_quality/service/PropertyService.java
@@ -1,6 +1,10 @@
 package com.betacampers.desafio_quality.service;
 
 import com.betacampers.desafio_quality.dto.RoomResponseDto;
+import com.betacampers.desafio_quality.model.Property;
+import com.betacampers.desafio_quality.model.Room;
+import com.betacampers.desafio_quality.repository.IPropertyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -9,6 +13,10 @@ import java.util.UUID;
 
 @Service
 public class PropertyService implements IPropertyService {
+
+    @Autowired
+    private IPropertyRepository propertyRepository;
+
     @Override
     public Double getPropertyArea(UUID propertyId) {
         return null;
@@ -16,7 +24,14 @@ public class PropertyService implements IPropertyService {
 
     @Override
     public BigDecimal getPropertyValue(UUID propertyId) {
-        return null;
+        Property property = propertyRepository.getById(propertyId);
+
+        double m2 = 0;
+        for(Room r:property.getPropRooms()){
+            m2 += r.getRoomLength() * r.getRoomWidth();
+        }
+
+        return property.getPropDistrict().getValueDistrictM2().multiply(new BigDecimal(m2));
     }
 
     @Override
