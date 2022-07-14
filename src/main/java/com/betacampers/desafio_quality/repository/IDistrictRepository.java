1a759a0cc4b924b3d196345e1e2b03d4b619d883
˜Thiago <thiago.sordi@mercadolivre.com>
2022-07-14 17:34:40 -0300

    Javadocs dos services e repositories

--- src/main/java/com/betacampers/desafio_quality/repository/IDistrictRepository.java
+++ src/main/java/com/betacampers/desafio_quality/repository/IDistrictRepository.java
@@ -3,7 +3,17 @@ package com.betacampers.desafio_quality.repository;
 import com.betacampers.desafio_quality.model.District;
 
 public interface IDistrictRepository {
+    /**
+     * Metódo do repository responsável por retornar um bairro dado um id.
+     * @param districtId - Long que representa o id do bairro
+     * @return Objeto District.
+     */
     District getById(Long districtId);
 
+    /**
+     * Metódo do repository responsável por salvar um bairro na base de dados.
+     * @param district - objeto District que será salvo
+     * @return Objeto District que foi salvo salvo.
+     */
     District save(District district);
 }
--- src/main/java/com/betacampers/desafio_quality/repository/IPropertyRepository.java
+++ src/main/java/com/betacampers/desafio_quality/repository/IPropertyRepository.java
@@ -5,9 +5,24 @@ import com.betacampers.desafio_quality.model.Property;
 import java.util.List;
 
 public interface IPropertyRepository {
+
+    /**
+     * Metódo do repository responsável por retornar um imóvel dado um id.
+     * @param propertyId - Long que representa o id do imóvel
+     * @return Objeto Property.
+     */
     Property getById(Long propertyId);
 
+    /**
+     * Metódo do repository responsável por retornar uma lista de imóveis.
+     * @return Uma lista de Property.
+     */
     List<Property> getAll();
 
+    /**
+     * Metódo do repository responsável por salvar um imóvel na base de dados.
+     * @param property - objeto Property que será salvo
+     * @return Objeto Property que foi salvo salvo.
+     */
     Property save(Property property);
 }
